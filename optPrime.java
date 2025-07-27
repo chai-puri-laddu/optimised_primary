d85cf6214cd9f3f989a3e015acfe39677bc1d2e4
karthik chepuri killer <killerspace66@gmail.com>
2025-07-27 21:59:07 +0530

    optimised-prime

---
+++ optPrime.java
@@ -0,0 +1,27 @@
+ import java.util.*;
+public class optPrime {
+    public static boolean isPrime(int n ){
+        n = (int) n;
+        int limit =  (int) Math.sqrt(n);
+        if(n<=1) return false;
+        if(n<=3) return true;
+        if(n%2==0 || n%3==0) return false;
+        for(int i=5;i<=limit;i+=6){//every prime number is of the from of 6K+-1}
+        if(n%i==0 || n%(i+2)==0) return false;
+        
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(isPrime(n)){
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");    
+
+        
+    }
+    
+}
+}
